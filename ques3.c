22124aae85587dd3d2fac37da377ea7c9e827b0e
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-05-29 19:30:36 +0530

    Create ques3.c

---
+++ ques3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of data types in bytes:\n");
+    
+    printf("char:                %f\n", sizeof(char));
+    printf("short int:           %f\n", sizeof(short int));
+    printf("int:                 %f\n", sizeof(int));
+    printf("long int:            %f\n", sizeof(long int));
+    printf("long long int:       %f\n", sizeof(long long int));
+    printf("float:               %f\n", sizeof(float));
+    printf("double:              %f\n", sizeof(double));
+    printf("long double:         %f\n", sizeof(long double));
+
+    return 0;
+  
+}
