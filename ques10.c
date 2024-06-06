e2b8374610fe2b0235ff08d576fbbd6ffeb9dfd6
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-06-06 14:06:39 +0530

    Create ques10.c

---
+++ ques10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int base, exponent, result = 1;
+
+    printf("Enter the base: ");
+    scanf("%d", &base);
+
+    printf("Enter the exponent: ");
+    scanf("%d", &exponent);
+
+    for (int i = 1; i <= exponent; i++)
+    {
+        result *= base;
+    }
+
+    printf("%d raised to the power of %d is: %d\n", base, exponent, result);
+
+    return 0;
+}
