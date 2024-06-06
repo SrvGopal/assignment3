c247b1f571f23b1ec49b8f2bfef1eb2ad7e3c567
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-06-06 14:11:10 +0530

    Update ques10.c

--- ques10.c
+++ ques10.c
@@ -4,16 +4,15 @@ int main(){
     long long int base, exponent, result = 1;
 
     printf("Enter the base: ");
-    scanf("%d", &base);
+    scanf("%lld", &base);
 
     printf("Enter the exponent: ");
-    scanf("%d", &exponent);
+    scanf("%lld", &exponent);
     int i;
     for ( i = 1; i <= exponent; i++){
         result *= base;
     }
 
-    printf("%d raised to the power of %d is: %d\n", base, exponent, result);
+    printf("%lld raised to the power of %lld is: %lld\n", base, exponent, result);
 
-    return 0;
 }
