1b281144cbb069dd3339b91c6dea3df161c54e6b
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-06-07 19:39:36 +0530

    Update ques13.c

--- ques13.c
+++ ques13.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 
-int main()
-{
-    int num, sum = 0;
-
-    printf("Enter any positive number: ");
+int main() {
+    int num;
+    printf("Enter a positive number: ");
     scanf("%d", &num);
 
-    for (int i = 1; i < num; i++)
-    {
-        if (num % i == 0)
-        {
+    int sum = 0;
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0) {
             sum += i;
         }
     }
 
-    if (sum == num)
-    {
+    if (sum == num) {
         printf("%d is a perfect number.\n", num);
-    }
-    else
-    {
+    } else {
         printf("%d is not a perfect number.\n", num);
     }
 
